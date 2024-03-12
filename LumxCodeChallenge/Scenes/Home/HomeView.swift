d78f6e8f6dce6a958dbd686293db74e280266f55
Luiz Carrion <ebc.luiz@gmail.com>
2024-03-11 23:38:25 -0300

    view and viewcontroller

--- LumxCodeChallenge/Scenes/Home/HomeView.swift
+++ LumxCodeChallenge/Scenes/Home/HomeView.swift
@@ -8,5 +8,47 @@
 import UIKit
 
 class HomeView: BaseView {
-  
+
+  //MARK: - Views
+  lazy var collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .vertical
+    layout.itemSize = CGSize(width: 100, height: 100)
+
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.backgroundColor = .clear
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+    collectionView.register(ImageTextCollectionViewCell.self, forCellWithReuseIdentifier: String(describing: ImageTextCollectionViewCell.self ))
+
+    return collectionView
+  }()
+
+  lazy var segmentedControl: UISegmentedControl = {
+    let items = ["Upcoming", "Popular"]
+    let segmentedControl = UISegmentedControl(items: items)
+    segmentedControl.selectedSegmentIndex = 0
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    return segmentedControl
+  }()
+
+
+  //MARK: - Setup
+
+  override func setup() {
+    addSubview(collectionView)
+    addSubview(segmentedControl)
+  }
+
+  override func setupConstraints() {
+    NSLayoutConstraint.activate([
+      segmentedControl.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+      segmentedControl.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+      collectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
+      collectionView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+      collectionView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+      collectionView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor)
+    ])
+  }
 }
--- LumxCodeChallenge/Scenes/Home/HomeViewController.swift
+++ LumxCodeChallenge/Scenes/Home/HomeViewController.swift
@@ -7,6 +7,70 @@
 
 import UIKit
 
+protocol HomeViewControllerDelegate: AnyObject {
+  func updateCollectionView()
+  func displayError(error: Error)
+}
+
 class HomeViewController: BaseViewController<HomeView> {
-  
+
+  //MARK: - ViewModel
+
+  let viewModel: HomeViewModelProtocol
+
+  //MARK: - Init
+
+  init(viewModel: HomeViewModelProtocol) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+    setup()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  //MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    viewModel.loadUpcomingMovies()
+  }
+
+  func setup() {
+    baseView.collectionView.delegate = self
+    baseView.collectionView.dataSource = self
+  }
+}
+
+extension HomeViewController: HomeViewControllerDelegate {
+  func displayError(error: Error) {
+    print("error")
+  }
+
+  func updateCollectionView() {
+    print("request")
+  }
+}
+
+//MARK: - CollectionView Delegate
+
+extension HomeViewController: UICollectionViewDelegate {
+
+}
+
+//MARK: - CollectionView DataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ImageTextCollectionViewCell.self), for: indexPath) as? ImageTextCollectionViewCell else {
+      return UICollectionViewCell()
+    }
+    cell.configure(image: nil, title: "teste", text: "teste")
+    return cell
+  }
 }
